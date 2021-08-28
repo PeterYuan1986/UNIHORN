fd113619baa1ca16528f01f40dbdc7ae23d8daa5
PeterYuan1986 <hongyuyuan86@gmail.com>
2021-08-28 11:50:57 -0400

    first commit

---
+++ newEmptyPHP.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//$ss= array();
+$ss['ss']=0;
+$ss['s2']=0;
+print_r($ss);
